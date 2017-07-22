f00c464a8b37ca10e39ce485668ee20521de273f
Maciej Brencz <maciej.brencz@gmail.com>
2017-07-22 18:01:07 +0200

    Handle more TO-? packages (thanks to jedec.org list)

--- build_regex.sh
+++ build_regex.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # TO packages
-TO_PACKAGES=`curl -s 'http://www.icpackage.org/TO-65/' curl -s 'http://www.interfacebus.com/semiconductor-transistor-packages.html'`
+TO_PACKAGES=`curl -s 'http://www.icpackage.org/TO-65/' curl -s 'http://www.interfacebus.com/semiconductor-transistor-packages.html' curl -s 'https://www.jedec.org/standards-documents/focus/registered-outlines-jep95/transistor-outlines-archive'`
 
 echo $TO_PACKAGES | grep -oE 'TO-[0-9A-Z]+' | grep -vE '92|220|236|247|252|261|263|277' | sort | uniq | cut -d'-' -f 2 | sort -n | tr "\n" '|' | sed 's/|$//g' | awk '{print "TO-?("$1")"}'
 
