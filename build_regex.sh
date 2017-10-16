f7423063b038656a0735c27bebb9c1067172e379
Maciej Brencz <maciej.brencz@gmail.com>
2017-10-16 18:14:15 +0200

    SOT packages | add a list from nxp.com

--- build_regex.sh
+++ build_regex.sh
@@ -9,3 +9,8 @@ echo $TO_PACKAGES | grep -oE 'TO-[0-9A-Z]+' | grep -vE '92|220|236|247|252|261|2
 SLIP_PACKAGES=`curl -s 'https://elecena.pl/search.json?q=SILP'`
 
 echo $SLIP_PACKAGES | grep -oE 'SILP-[0-9]+' | sort | uniq | cut -d'-' -f 2 | sort -n | tr "\n" '|' | sed 's/|$//g' | awk '{print "SILP-?("$1")"}'
+
+# SOT packages
+SOT_PACKAGES=`curl -s 'https://www.nxp.com/packages/search?q=SOT&type=0'`
+
+echo $SOT_PACKAGES | grep -oE 'SOT[0-9]+' | sort | uniq | sed 's/SOT//g' | sort -n | tr "\n" '|' | sed 's/|$//g' |  awk '{print "SOT-?("$1")"}'
